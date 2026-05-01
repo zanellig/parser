8c11dd5c12b3ecd65347f246f340481c7d382eb1
zanellig <gonzalozanelli1@gmail.com>
2026-05-01 02:17:37 -0300

    i'm going cray cray

--- index.ts
+++ index.ts
@@ -3,21 +3,23 @@ import path from "path"
 
 class InvalidParameterError extends Error {}
 class RequiredPathFlagError extends Error {}
-class RequiredPathError extends Error {}
 
 interface Flag extends Object {
     flagName: string,
     acceptsParam: boolean,
-    paramType?: string,
-    param?: string
+}
+
+interface ParamFlag extends Flag {
+    paramType: string,
+    param: string
 }
 
 /**
  * Just for shits and giggles
  */
 class ArgumentReader {
-    #parsed: Array<Flag> = []
-    #ignored: Array<Flag> = []
+    #parsed:  Array<Flag | ParamFlag> = []
+    #ignored: Array<Flag | ParamFlag> = []
     #executablePath = ""
     #scriptPath = ""
 
@@ -52,45 +54,112 @@ class ArgumentReader {
     /**
      * @throws {InvalidParameterError}
      */
-    #parseFlag(flag: string, parameter: string): Flag {
+    #parseFlag(flag: string, parameter: string): Flag | ParamFlag {
         switch (flag) {
             case "--path":
             case "--file":
+            case "-p":
+            case "-f":
                 if (typeof parameter !== "string" || !parameter) throw new InvalidParameterError
                 return {
                     flagName: "path",
                     acceptsParam: true,
                     paramType: "string",
                     param: parameter
-                }
+                } as ParamFlag
             default:
                 return {
                     flagName: flag.replaceAll("-", ""),
                     acceptsParam: false,
-                }
+                } as Flag
+            }
+        }
+        
+        get(arg: string) {
+            return this.#parsed.find((v) => v.flagName === arg)
+        }
+    }
+    
+class InvalidParsedNumber extends Error {}
+
+interface Action extends Object {
+    actionName: HumanReadableActions
+}
+
+interface RepeateableAction extends Action {
+    reps: number
+}
+
+interface ParameterizableAction extends Action {
+    param: number
+}
+
+type Actions = {
+    "P":    ParameterizableAction
+    "D":    Action
+    "N":    RepeateableAction
+    "S":    RepeateableAction
+    "E":    RepeateableAction
+    "W":    RepeateableAction
+    "U":    Action
+}
+
+interface PreparsedParts extends Object {
+    a: string | undefined
+    s: string | undefined
+    n: string | undefined
+}
+
+enum HumanReadableActions {
+    "P" = "select_pen",
+    "D" = "pen_down",
+    "N" = "move_north",
+    "S" = "move_south",
+    "E" = "move_east",
+    "W" = "move_west",
+    "U" = "pen_up"
+}
+
+class ActionParser {
+    #actions: Array<Actions> = []
+
+    constructor(lines: Array<string>) {
+        for (const line of lines) {
+            const parts: PreparsedParts = { a: line[0], s: line[1], n: line[2] }
+            if (!parts.a) continue
+            if (parts.s !== " ") {
+                delete parts.s
+                delete parts.n
+            }
+            console.log(parts)
         }
     }
 
-    get(arg: string) {
-        return this.#parsed.find((v) => v.flagName === arg)
+    #parseNumber() {}
+
+    /** Este getter es al pedo por ahora, pero bueno */
+    get actions() {
+        return this.#actions
     }
 }
 
+// {action}{space}{number}
 
 async function main() {
     const args = new ArgumentReader(process.argv)
-    const pathFlag = args.get("path")
+    const pathFlag = args.get("path") as ParamFlag
     if (!pathFlag) throw new RequiredPathFlagError
-    if (!pathFlag.param) throw new RequiredPathError
     const file = fs.readFileSync(
         path.join(
             process.env.PWD,
             pathFlag.param
         ),
-        {
-            encoding: "UTF-8"
-        }
+        "utf-8"
     )
+    const lines = file.split("\n")
+
+    const lineParser = new ActionParser(lines)
+    
 }
 
 main()
